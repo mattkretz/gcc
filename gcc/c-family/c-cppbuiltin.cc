89e7e6f91a511c39bc2acbf7b7ec7d3a87e9120e
Matthias Kretz <m.kretz@gsi.de>
2019-07-25 17:07:59 +0200

    c++: Allow non-member subscript overloads
    gcc/c-family/ChangeLog:
    
    	* c-cppbuiltin.cc (c_cpp_builtins): Pre-define
    	__GXX_NONMEMBER_SUBSCRIPT__ to 1.
    
    gcc/cp/ChangeLog:
    
    	* call.cc (add_operator_candidates): Remove restriction for
    	operator[] to be a non-static member function.
    	* decl.cc (grok_op_properties): Ditto.

--- gcc/c-family/c-cppbuiltin.cc
+++ gcc/c-family/c-cppbuiltin.cc
@@ -943,6 +943,8 @@ c_cpp_builtins (cpp_reader *pfile)
 
   if (c_dialect_cxx ())
     {
+      cpp_define (pfile, "__GXX_NONMEMBER_SUBSCRIPT__=1");
+
       if (flag_weak && SUPPORTS_ONE_ONLY)
 	cpp_define (pfile, "__GXX_WEAK__=1");
       else
