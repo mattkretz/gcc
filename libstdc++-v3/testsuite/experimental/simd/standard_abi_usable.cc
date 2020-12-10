b8c5c2f9b2f31f7eb26300c2b1a8272f522abae3
Matthias Kretz <kretz@kde.org>
2020-12-10 17:21:59 +0100

    Test fixed_size usability and add copyright
    libstdc++-v3/ChangeLog:
    	* testsuite/experimental/simd/standard_abi_usable.cc: Add tests
    	for fixed_size_simd<T, 3> and fixed_size_simd<T, max>. Add
    	copyright header.

--- libstdc++-v3/testsuite/experimental/simd/standard_abi_usable.cc
+++ libstdc++-v3/testsuite/experimental/simd/standard_abi_usable.cc
@@ -1,45 +1,64 @@
 // { dg-options "-std=c++17 -fno-fast-math" }
-// { dg-do compile }
+// { dg-do compile { target c++17 } }
+
+// Copyright (C) 2020 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
 
 #include <experimental/simd>
 
-template <class T>
+template <typename V>
   void
   is_usable()
   {
-    static_assert(std::is_default_constructible_v<std::experimental::simd<T>>);
-    static_assert(std::is_destructible_v<std::experimental::simd<T>>);
-    static_assert(
-      std::is_default_constructible_v<std::experimental::native_simd<T>>);
-    static_assert(std::is_destructible_v<std::experimental::native_simd<T>>);
+    static_assert(std::is_default_constructible_v<V>);
+    static_assert(std::is_destructible_v         <V>);
+    static_assert(std::is_default_constructible_v<typename V::mask_type>);
+    static_assert(std::is_destructible_v         <typename V::mask_type>);
+  }
 
-    static_assert(
-      std::is_default_constructible_v<std::experimental::simd_mask<T>>);
-    static_assert(std::is_destructible_v<std::experimental::simd_mask<T>>);
-    static_assert(
-      std::is_default_constructible_v<std::experimental::native_simd_mask<T>>);
-    static_assert(
-      std::is_destructible_v<std::experimental::native_simd_mask<T>>);
+template <typename T>
+  void
+  test01()
+  {
+    namespace stdx = std::experimental;
+    is_usable<stdx::simd<T>>();
+    is_usable<stdx::native_simd<T>>();
+    is_usable<stdx::fixed_size_simd<T, 3>>();
+    is_usable<stdx::fixed_size_simd<T, stdx::simd_abi::max_fixed_size<T>>>();
   }
 
 int main()
 {
-  is_usable<char>();
-  is_usable<wchar_t>();
-  is_usable<char16_t>();
-  is_usable<char32_t>();
+  test01<char>();
+  test01<wchar_t>();
+  test01<char16_t>();
+  test01<char32_t>();
 
-  is_usable<signed char>();
-  is_usable<unsigned char>();
-  is_usable<short>();
-  is_usable<unsigned short>();
-  is_usable<int>();
-  is_usable<unsigned int>();
-  is_usable<long>();
-  is_usable<unsigned long>();
-  is_usable<long long>();
-  is_usable<unsigned long long>();
-  is_usable<float>();
-  is_usable<double>();
-  is_usable<long double>();
+  test01<signed char>();
+  test01<unsigned char>();
+  test01<short>();
+  test01<unsigned short>();
+  test01<int>();
+  test01<unsigned int>();
+  test01<long>();
+  test01<unsigned long>();
+  test01<long long>();
+  test01<unsigned long long>();
+  test01<float>();
+  test01<double>();
+  test01<long double>();
 }
